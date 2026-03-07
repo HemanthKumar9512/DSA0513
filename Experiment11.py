bf36fcd7237d3eead6f74a08717384967d2ebb8e
Hemanth Kumar S <venki009512@gmail.com>
2026-03-07 14:14:08 +0530

    Create Experiment11.py

---
+++ Experiment11.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+
+df = pd.DataFrame(np.random.randn(10,4), columns=list('ABCD'))
+
+df.iloc[2,1] = np.nan
+df.iloc[5,3] = np.nan
+
+def highlight_nan(val):
+    return 'background-color: red' if pd.isna(val) else ''
+
+df.style.applymap(highlight_nan)
