a286ae5d89e0bd2dc4f7dfe7d82ded65781b713a
Hemanth Kumar S <venki009512@gmail.com>
2026-03-12 14:47:33 +0530

    Create Experiment24.py

---
+++ Experiment24.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+days=[1,2,3,4,5]
+price=[800,820,830,810,840]
+
+plt.plot(days,price)
+plt.xlabel("Days")
+plt.ylabel("Stock Price")
+plt.title("Alphabet Inc Stock Trend")
+plt.show()
