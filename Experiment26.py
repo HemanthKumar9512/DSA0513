9a19d75d22dda931a7668a5923e89938bd521935
Hemanth Kumar S <venki009512@gmail.com>
2026-03-12 14:48:12 +0530

    Create Experiment26.py

---
+++ Experiment26.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+plt.subplot(1,2,1)
+plt.plot([1,2,3],[2,4,6])
+plt.title("Plot1")
+
+plt.subplot(1,2,2)
+plt.plot([1,2,3],[3,6,9])
+plt.title("Plot2")
+
+plt.show()
